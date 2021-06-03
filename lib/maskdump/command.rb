b734bb59f88dfafef161a4a2921fa08e1f881ffa
kohtaro24 <koutaro.nishi.fb@gmail.com>
2021-06-04 00:10:04 +0900

    Support --verpose option

--- lib/maskdump/command.rb
+++ lib/maskdump/command.rb
@@ -9,15 +9,23 @@ module Maskdump
     class_option :user,     type: :array,  aliases: "-u", banner: 'user'
     class_option :password, type: :string, aliases: "-p", banner: 'password'
     class_option :output,   type: :string, aliases: "-o", banner: 'output', required: true
+    class_option :verbose,  type: :boolean, aliases: "-v", banner: 'verbose'
 
     desc "dump", "dump"
     def dump(yaml)
       @setting = Setting.new(yaml, options)
       connect_db
       ActiveRecord::Tasks::DatabaseTasks.structure_dump(@setting.db_setting_hash, options[:output])
+      progress = 0
+      tables_size = @setting.tables.size
       @setting.tables.each do |table_setting|
+        progress += 1
         table = Table.new(table_setting[:name])
-        next if table.records.blank?
+        if table.records.blank?
+          puts "\e[0;37m(#{progress}/#{tables_size}) [#{table_setting[:name]}] Skipped. empty records.\e[0m" if options[:verbose]
+          next
+        end
+        print "(#{progress}/#{tables_size}) \e[1;32m[#{table_setting[:name]}]\e[0m Start dump records..." if options[:verbose]
         masked_records = Mask.new(table.records, table_setting[:columns]).mask
         query = Query.generate_insert_statements(table.name, masked_records, table.columns, @setting)
         File.open(options[:output], 'a') do |f|
@@ -25,6 +33,7 @@ module Maskdump
           f.puts query
           f.puts "\n"
         end
+        puts " Finished." if options[:verbose]
       end
     end
 
